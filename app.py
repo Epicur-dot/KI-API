350e62df75008baaa28bef7a8edc60efc8532fd7
Epicur <Leander.Huggenberger@edu.bib.de>
2025-05-21 12:19:32 +0200

    Neue API sachen hinzugefügt

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import requests
 import json
 
-API_Key = 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
+API_Key = "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"
+API = "https://api.groq.com/openai/v1/models"
+
+headers  ={
+    "Authorization":f"Bearer {API_Key}",
+    "Content-Type":"application/json"
+}
+payload ={
+    "model":""
+}
+response = requests.request("GET",API,headers=headers)
+print(response.content)
